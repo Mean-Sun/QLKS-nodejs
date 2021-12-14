58385ab3e5ed7a6b6f54e45665f844de8d33ca1a
Mean-Sun <tranminhsonyi@gmail.com>
2021-12-14 21:28:57 +0700

    update using mySQL

--- index.js
+++ index.js
@@ -3,15 +3,9 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
 //create app
 const app = express();
-
-//config view engine
-const hbs = exphbs.create({
-    defaultLayout: 'layout',
-    extname: '.hbs'
-});
+//const { sequelize } = require('./models');
 
 const authRouter = require('./routes/authRoute');
 const homeRouter = require('./routes/homeRoute');
@@ -19,14 +13,21 @@ const rentRoomRouter = require('./routes/rentRoomRoute');
 const typeofRoomRouter = require('./routes/typeofRoomRouter');
 const roomRouter = require('./routes/roomRouter');
 
+
+//config view engine
+const hbs = exphbs.create({
+    defaultLayout: 'layout',
+    extname: '.hbs'
+});
+
 //use view engine
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
 
+
 app.use(bodyParser.urlencoded({ extended: false }))
     // parse application/json
 app.use(bodyParser.json())
-
 
 //set static folder
 app.use(express.static(path.join(__dirname, '/publics/')));
@@ -37,9 +38,25 @@ app.use('/typeofRoom', typeofRoomRouter);
 app.use('/room', roomRouter);
 app.use('/', authRouter);
 
-
-// catch 404 and forward to error handler
-//TODO
-//app.use('*', (req, res) => res.render('404', { layout: '404' }))
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
+
+
+
+/* app.get('/', (req, res) => {
+    res.render('rent-room', { layout: 'main' });
+    sequelize.authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+            // catch 404 and forward to error handler
+            //TODO
+            //app.use('*', (req, res) => res.render('404', { layout: '404' }))
+
+            const port = process.env.PORT || 3000;
+            app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
+        })
+        .catch(error => {
+            console.error('Unable to connect to the database:', error);
+        });
+
+}) */
--- model/model.js
+++ model/model.js
@@ -1,24 +1,24 @@
 var mysql = require('mysql');
 
 //online data base, mysql on Cpanel
-/* var pool = mysql.createPool({
+var pool = mysql.createPool({
     connectionLimit: 2000,
     host: "testlogin.xyz",
     user: "teslogin_tester_meansun",
     password: "meansun123456",
     database: "teslogin_qlks",
-}); */
+});
 
 
 
 //localhost data bae php admin
-var pool = mysql.createPool({
+/* var pool = mysql.createPool({
     connectionLimit: 2000,
     host: "localhost",
     user: "testqlks",
     password: "123456",
     database: "qlks",
-});
+}); */
 
 
 //export pool
